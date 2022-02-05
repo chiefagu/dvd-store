590151abb9af47cfd570d5bb85bd1d6f99c9a490
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-02-05 23:05:13 +0100

    feat(middleware/not-found): Add not-found #13

--- src/middleware/index.mjs
+++ src/middleware/index.mjs
@@ -1,4 +1,6 @@
 import { logger } from "../logger/index.mjs";
 import { makeErrorHandler } from "./error.middleware.mjs";
+import { makeNotFoundHandler } from "./not-found.middleware.mjs";
 
 export const errorHandler = makeErrorHandler(logger);
+export const notFoundHandler = makeNotFoundHandler(logger);
