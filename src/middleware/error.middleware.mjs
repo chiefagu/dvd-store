07f0224ad3ed41c977b3a81b5b76d3f134434f95
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-02-05 23:55:20 +0100

    fix:(middleware/error): add status code with response

--- src/middleware/error.middleware.mjs
+++ src/middleware/error.middleware.mjs
@@ -1,6 +1,6 @@
 export function makeErrorHandler(logger) {
   return function errorHandler(err, req, res) {
     logger.warn(err.message, err);
-    return res.send("something went wrong");
+    return res.status(500).send("something went wrong");
   };
 }
