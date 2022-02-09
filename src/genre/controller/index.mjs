e36eb661c26099444f0224b9bac8fd9fd2a48229
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-02-09 16:13:05 +0100

    feat(controller/put-genre): Implement put-genre #67

--- src/genre/controller/index.mjs
+++ src/genre/controller/index.mjs
@@ -1,9 +1,12 @@
 import { makePostGenre } from "./post-genre.mjs";
 import { makeGetGenres } from "./get-genres.mjs";
+import { makePutGenre } from "./put-genre.mjs";
 
 import { logger } from "../../logger/index.mjs";
 import { addGenre } from "../use-case/index.mjs";
 import { listGenres } from "../use-case/index.mjs";
+import { editGenre } from "../use-case/index.mjs";
 
 export const postGenre = makePostGenre({ addGenre, logger });
 export const getGenres = makeGetGenres({ listGenres, logger });
+export const putGenre = makePutGenre({ editGenre, logger });
