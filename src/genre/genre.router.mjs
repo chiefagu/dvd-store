0abf4fa811ae2a4fdc4b44f30c99073ebff29244
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-02-09 12:06:13 +0100

    feat(genre-router): Add get-router #60

--- src/genre/genre.router.mjs
+++ src/genre/genre.router.mjs
@@ -1,11 +1,12 @@
 import express from "express";
 import { expressCallBack } from "../express-callback/index.mjs";
-import { postGenre } from "./controller/index.mjs";
+import { getGenres, postGenre } from "./controller/index.mjs";
 
 export function genreRouter() {
   const router = express.Router();
 
   router.post("/", expressCallBack(postGenre));
+  router.get("/", expressCallBack(getGenres));
 
   return router;
 }
