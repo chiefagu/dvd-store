b4c9168299e5cd1d3a40135c9615d94d41a5151c
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-02-09 16:20:06 +0100

    feat(genre-router): Add put to genre-router #68

--- src/genre/genre.router.mjs
+++ src/genre/genre.router.mjs
@@ -1,12 +1,13 @@
 import express from "express";
 import { expressCallBack } from "../express-callback/index.mjs";
-import { getGenres, postGenre } from "./controller/index.mjs";
+import { getGenres, postGenre, putGenre } from "./controller/index.mjs";
 
 export function genreRouter() {
   const router = express.Router();
 
   router.post("/", expressCallBack(postGenre));
   router.get("/", expressCallBack(getGenres));
+  router.put("/:id", expressCallBack(putGenre));
 
   return router;
 }
