05c89049cf05e69f780b9de34ec3d81a56ebfd29
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-02-09 15:30:38 +0100

    feat(use-case/edit-genre): Implement use-case #66

--- src/genre/use-case/index.mjs
+++ src/genre/use-case/index.mjs
@@ -1,6 +1,9 @@
 import { makeAddGenre } from "./add-genre.mjs";
 import { makeListGenres } from "./list-genres.mjs";
+import { makeEditGenre } from "./edit-genre.mjs";
 import { genreDb } from "../data-access/index.mjs";
+import { Id } from "../../utils/Id.mjs";
 
 export const addGenre = makeAddGenre({ genreDb });
 export const listGenres = makeListGenres({ genreDb });
+export const editGenre = makeEditGenre({ Id, genreDb });
