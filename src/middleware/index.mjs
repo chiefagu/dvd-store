eb14d74fb441b0f20c6b25b085d42ea3a5e8e2af
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-02-13 11:19:59 +0100

    feat(middleware/deserialize-user): require a user obj
    > ensures only authenticated users are allowed to proceed
    > extract user info from decoded jwt token
    
    #Closes 79

--- src/middleware/index.mjs
+++ src/middleware/index.mjs
@@ -1,6 +1,14 @@
 import { logger } from "../logger/index.mjs";
+import { makeDeserializeUser } from "./deserialize-user.mjs";
 import { makeErrorHandler } from "./error.middleware.mjs";
 import { makeNotFoundHandler } from "./not-found.middleware.mjs";
 
+import { verifyJwt, reIssueAcessToken } from "../utils/index.mjs";
+
 export const errorHandler = makeErrorHandler(logger);
 export const notFoundHandler = makeNotFoundHandler(logger);
+export const deserializeUser = makeDeserializeUser({
+  verifyJwt,
+  reIssueAcessToken,
+  logger,
+});
