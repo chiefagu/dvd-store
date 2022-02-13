e0757fdb0816fdaa6dbb06e338d2d2437554cba7
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-02-13 12:22:01 +0100

    fix(middleware/is-admin): use correct file extension

--- src/middleware/index.mjs
+++ src/middleware/index.mjs
@@ -2,7 +2,7 @@ import { logger } from "../logger/index.mjs";
 import { makeDeserializeUser } from "./deserialize-user.mjs";
 import { makeErrorHandler } from "./error.middleware.mjs";
 import { makeNotFoundHandler } from "./not-found.middleware.mjs";
-import { makeIsAdmin } from "./is-admin.js";
+import { makeIsAdmin } from "./is-admin.mjs";
 
 import { verifyJwt, reIssueAcessToken } from "../utils/index.mjs";
 
