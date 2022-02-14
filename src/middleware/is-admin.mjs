b4007ff3e1ef8eaa9f25f351394825f85e293bfd
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-02-14 15:17:03 +0100

    chore(middeware/is-admin): change error message
    forbidden/cant perform this action , is more fitting in this context(missing priviledges)

--- src/middleware/is-admin.mjs
+++ src/middleware/is-admin.mjs
@@ -1,7 +1,7 @@
 export function makeIsAdmin() {
   return function isAdmin(req, res, next) {
     if (!req?.user?.isAdmin) {
-      return res.status(403).json({ message: "unauthorized user" });
+      return res.status(403).json({ message: "Can't perform this action" });
     }
     return next();
   };
