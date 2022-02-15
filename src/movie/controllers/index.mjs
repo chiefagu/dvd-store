2706dd564060a5bf3108adc5e27bff060eb16034
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-02-15 16:14:37 +0100

    feat(controller/get-movies): Implement get controller #101

--- src/movie/controllers/index.mjs
+++ src/movie/controllers/index.mjs
@@ -1,6 +1,8 @@
 import { makePostMovie } from "./post-movie.mjs";
+import { makeGetMovies } from "./get-movies.mjs";
 
 import { logger } from "../../logger/index.mjs";
-import { addMovie } from "../use-case/index.mjs";
+import { addMovie, listMovies } from "../use-case/index.mjs";
 
 export const postMovie = makePostMovie({ addMovie, logger });
+export const getMovies = makeGetMovies({ listMovies, logger });
