97e7902fc576695431bb5527c003926451149bed
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-02-18 19:34:36 +0100

    feat(movie.router): Add put movie router #112

--- src/movie/movie.router.mjs
+++ src/movie/movie.router.mjs
@@ -1,13 +1,14 @@
 import express from "express";
 
 import { expressCallBack } from "../express-callback/index.mjs";
-import { getMovies, postMovie } from "./controllers/index.mjs";
+import { getMovies, postMovie, putMovie } from "./controllers/index.mjs";
 
 export function movieRouter() {
   const router = express.Router();
 
   router.post("/", expressCallBack(postMovie));
   router.get("/", expressCallBack(getMovies));
+  router.put("/:id", expressCallBack(putMovie));
 
   return router;
 }
