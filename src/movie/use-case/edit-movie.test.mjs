8560eab8ca91328fb423821e67f6a7170d69dcb2
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-02-18 16:03:46 +0100

    feat(uses-case/edit-movie): Implement edit-movie use-case
    > created edit-movie function
    > injected dependencies
    > added tests
    
    #110

---
+++ src/movie/use-case/edit-movie.test.mjs
@@ -0,0 +1,124 @@
+import { makeEditMovie } from "./edit-movie.mjs";
+import { Id } from "../../utils/index.mjs";
+
+describe("edit movie use-case", () => {
+  let editMovie;
+
+  const movieDb = { findById: jest.fn(), update: jest.fn() };
+  const genreDb = { findById: jest.fn() };
+
+  beforeAll(() => {
+    editMovie = makeEditMovie({ movieDb, genreDb, Id });
+  });
+
+  describe("supplied with invalid arguments", () => {
+    it("throws an error if the id is invalid", async () => {
+      const userInput = { id: "invalid", title: "a title" };
+
+      try {
+        await editMovie(userInput);
+        expect(movieDb.findById).not.toHaveBeenCalled();
+      } catch (error) {
+        expect(error.message).toMatchInlineSnapshot(
+          `"you must supply a valid id"`
+        );
+      }
+    });
+
+    it("throws an error if movie doesn't exist", async () => {
+      const userInput = { id: Id.makeId(), title: "a title" };
+
+      movieDb.findById.mockResolvedValueOnce(null);
+
+      try {
+        await editMovie(userInput);
+        expect(movieDb.findById).toHaveBeenCalledWith(userInput.id);
+        expect(movieDb.findById).toHaveBeenCalledTimes(1);
+      } catch (error) {
+        expect(error.message).toMatchInlineSnapshot(`"no such movie exists"`);
+      }
+    });
+
+    it("throws an error if genreId is invalid", async () => {
+      const userInput = {
+        id: Id.makeId(),
+        title: "a title",
+        genreId: "invalid",
+        numberInStock: 50,
+        dailyRentalRate: 10,
+      };
+
+      movieDb.findById.mockResolvedValueOnce({ ...userInput });
+
+      try {
+        await editMovie(userInput);
+      } catch (error) {
+        expect(error.message).toMatchInlineSnapshot(
+          `"Movie must have a valid genreId"`
+        );
+      }
+    });
+
+    it("throws an error if no genre exists", async () => {
+      const userInput = {
+        id: Id.makeId(),
+        title: "a title",
+        genreId: Id.makeId(),
+        numberInStock: 50,
+        dailyRentalRate: 10,
+      };
+
+      movieDb.findById.mockResolvedValueOnce({ ...userInput });
+
+      genreDb.findById.mockResolvedValueOnce(null);
+
+      try {
+        await editMovie(userInput);
+        expect(movieDb.findById).toHaveBeenCalledWith(userInput.id);
+        expect(movieDb.findById).toHaveBeenCalledTimes(1);
+
+        expect(genreDb.findById).toHaveBeenCalledWith(userInput.genreId);
+        expect(genreDb.findById).toHaveBeenCalledTimes(1);
+      } catch (error) {
+        expect(error.message).toMatchInlineSnapshot(`"no such genre exists"`);
+      }
+    });
+  });
+
+  describe("supplied valid arguments", () => {
+    it("successfully updates and returns the movie", async () => {
+      const userInput = {
+        id: Id.makeId(),
+        title: "A title",
+        genreId: Id.makeId(),
+        numberInStock: 50,
+        dailyRentalRate: 10,
+      };
+
+      movieDb.findById.mockResolvedValueOnce({ ...userInput });
+
+      const genre = { _id: Id.makeId(), name: "Action" };
+      genreDb.findById.mockResolvedValueOnce(genre);
+
+      try {
+        await editMovie(userInput);
+
+        expect(movieDb.findById).toHaveBeenCalledWith(userInput.id);
+        expect(movieDb.findById).toHaveBeenCalledTimes(1);
+
+        expect(genreDb.findById).toHaveBeenCalledWith(userInput.genreId);
+        expect(genreDb.findById).toHaveBeenCalledTimes(1);
+
+        expect(movieDb.update).toHaveBeenCalledWith({
+          title: userInput.title,
+          genre: { _id: genre._id, name: genre.name },
+          dailyRentalRate: userInput.dailyRentalRate,
+          numberInStock: userInput.numberInStock,
+        });
+        expect(movieDb.update).toHaveBeenCalledTimes(1);
+      } catch (error) {
+        expect(error.message).toBeNull();
+      }
+    });
+  });
+});
--- src/movie/use-case/index.mjs
+++ src/movie/use-case/index.mjs
@@ -1,5 +1,6 @@
 import { makeAddMovie } from "./add-movie.mjs";
 import { makeListMovies } from "./list-movies.mjs";
+import { makeEditMovie } from "./edit-movie.mjs";
 
 import { Id } from "../../utils/index.mjs";
 import { genreDb } from "../../genre/data-access/index.mjs";
@@ -7,3 +8,4 @@ import { movieDb } from "../data-access/index.mjs";
 
 export const addMovie = makeAddMovie({ Id, genreDb, movieDb });
 export const listMovies = makeListMovies({ movieDb });
+export const editMovie = makeEditMovie({ movieDb, genreDb, Id });
