be551f3614e0715e33dfcc0b69bbfcec112af85d
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-03-29 19:15:22 +0100

    feat(use-case/edit-customer): implement edit-customer use-case
    > implement use-case
    > add test
    
    #148

--- src/customer/use-cases/index.mjs
+++ src/customer/use-cases/index.mjs
@@ -1,7 +1,9 @@
 import { makeAddCustomer } from "./add-customer.mjs";
 import { makeListCustomers } from "./list-customers.mjs";
+import { makeEditCustomer } from "./edit-customer.mjs";
 
 import { customerDb } from "../data-access/index.mjs";
 
 export const addCustomer = makeAddCustomer({ customerDb });
 export const listCustomers = makeListCustomers({ customerDb });
+export const editCustomer = makeEditCustomer({ customerDb });
