79214fcd49373440598517a0f0c02bfc73ddc4a4
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-03-29 19:51:37 +0100

    feat(controllers/put-customer): Implement controller
    > implement put-customer
    > added test
    
    #149

--- src/customer/controllers/index.mjs
+++ src/customer/controllers/index.mjs
@@ -1,8 +1,14 @@
 import { makePostCustomer } from "./post-customer.mjs";
 import { makeGetCustomers } from "./get-customers.mjs";
 
-import { addCustomer, listCustomers } from "../use-cases/index.mjs";
+import {
+  addCustomer,
+  listCustomers,
+  editCustomer,
+} from "../use-cases/index.mjs";
 import { logger } from "../../logger/index.mjs";
+import { makePutCustomer } from "./put-customer.mjs";
 
 export const postCustomer = makePostCustomer({ addCustomer, logger });
 export const getCustomers = makeGetCustomers({ listCustomers, logger });
+export const putCustomer = makePutCustomer({ editCustomer, logger });
