dcc1e74c81a495cfb59794e72412e867264e93e4
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-03-29 09:59:37 +0100

    chore(customer/use-case): DI
    >make and export list-customers
    > modified test to reflect new path and function

--- src/customer/use-cases/index.mjs
+++ src/customer/use-cases/index.mjs
@@ -1,5 +1,7 @@
 import { makeAddCustomer } from "./add-customer.mjs";
+import { makeListCustomers } from "./list-customers.mjs";
 
 import { customerDb } from "../data-access/index.mjs";
 
 export const addCustomer = makeAddCustomer({ customerDb });
+export const listCustomers = makeListCustomers({ customerDb });
