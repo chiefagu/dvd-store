0ef7b0c21370b322bb147ae9879686c2e055b85e
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-03-29 11:58:00 +0100

    chore(controllers): DI for getCustomers
    > make getCustomers
    > add test to check it is defined

--- src/customer/controllers/index.mjs
+++ src/customer/controllers/index.mjs
@@ -1,6 +1,8 @@
 import { makePostCustomer } from "./post-customer.mjs";
+import { makeGetCustomers } from "./get-customers.mjs";
 
-import { addCustomer } from "../use-cases/index.mjs";
+import { addCustomer, listCustomers } from "../use-cases/index.mjs";
 import { logger } from "../../logger/index.mjs";
 
 export const postCustomer = makePostCustomer({ addCustomer, logger });
+export const getCustomers = makeGetCustomers({ listCustomers, logger });
--- src/customer/controllers/index.test.mjs
+++ src/customer/controllers/index.test.mjs
@@ -1,9 +1,14 @@
-import { postCustomer } from "./index.mjs";
+import { postCustomer, getCustomers } from "./index.mjs";
 
 describe("controllers: DI", () => {
   describe("make post controller", () => {
     it("should be defined", () => {
       expect(postCustomer).toBeDefined();
+    });
+  });
+  describe("make getCustomers controller", () => {
+    it("should be defined", () => {
+      expect(getCustomers).toBeDefined();
     });
   });
 });
