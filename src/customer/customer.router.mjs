424d271baed2918750a99d68e20379a37c53c5b6
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-03-29 20:15:24 +0100

    chore(customer.router): add customer put-route
    #150

--- src/customer/customer.router.mjs
+++ src/customer/customer.router.mjs
@@ -1,13 +1,18 @@
 import { Router } from "express";
 
 import { expressCallBack } from "../express-callback/index.mjs";
-import { getCustomers, postCustomer } from "./controllers/index.mjs";
+import {
+  getCustomers,
+  postCustomer,
+  putCustomer,
+} from "./controllers/index.mjs";
 
 export function customerRouter() {
   const router = Router();
 
   router.post("/", expressCallBack(postCustomer));
   router.get("/", expressCallBack(getCustomers));
+  router.put("/:id", expressCallBack(putCustomer));
 
   return router;
 }
