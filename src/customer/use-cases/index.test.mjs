fe50aa7539772a1a94120150279249ba97596178
chiefagu <58363425+chiefagu@users.noreply.github.com>
2022-03-29 09:59:37 +0100

    chore(customer/use-case): DI
    >make and export list-customers
    > modified test to reflect new path and function

--- src/customer/use-cases/index.test.mjs
+++ src/customer/use-cases/index.test.mjs
@@ -1,9 +1,14 @@
-import { addCustomer } from "./index.mjs";
+import { addCustomer, listCustomers } from "./index.mjs";
 
 describe("use-case: DI", () => {
   describe("make add customer", () => {
     it("should be defined", () => {
       expect(addCustomer).toBeDefined();
+    });
+  });
+  describe("make list customers", () => {
+    it("should be defined", () => {
+      expect(listCustomers).toBeDefined();
     });
   });
 });
